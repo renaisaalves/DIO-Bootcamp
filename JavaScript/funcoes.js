04ea6945c8ab6b0b1c2c1eab3678227f8f932a4e
Renaísa <renaisaalves@gmail.com>
2022-07-02 07:58:31 -0300

    Create funcoes.js

---
+++ JavaScript/funcoes.js
@@ -0,0 +1,14 @@
+// FUNÇÕES
+
+/* Vamos conhecer uma estrutura básica de funções em JavaScript. 
+   Os parênteses em soma() servem para declarar atributos (parâmetros).
+*/
+
+function soma(a, b) {
+    return a + b;
+}
+
+/* Reparem que temos a nossa função prontinha. Ela serve para somar dois valores, os quais serão informados.
+   - O comando "return" serve para retornar o resultado da instrução que foi dada na função, mas não tem o caráter de exibir na tela esse resultado. 
+   - Para imprimir na tela alguma coisa, usamos no JavaScript o comando console.log(), que tem a mesma função do print() no Python.
+*/
