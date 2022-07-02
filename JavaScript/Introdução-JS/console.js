0d347d9491ddcccabb77d6379362873b31a022d8
Renaísa <renaisaalves@gmail.com>
2022-07-02 08:42:19 -0300

    Update console.js

--- JavaScript/Introdução-JS/console.js
+++ JavaScript/Introdução-JS/console.js
@@ -3,4 +3,6 @@
 Quem disse que o JavaScript só pode ser usado em uma IDE? JavaScript é a linguagem do navegador, portanto, ela pode ser usada diretamente no navegador! 
 
 Para usar a linguagem JavaScript no navegador (vou usar o Google Chrome), basta clicar em "Inspecionar" e depois em "Console". Dessa forma, você pode interagir com a linguagem diretamente no browser, declarando variáveis, constantes, funções, enfim... tudo o que quiser. E o melhor de tudo é que a interpretação ocorre na hora, já que os navegadores são fluentes nessa linguagem.
+
+Porém, vale ressaltar que o JavaScript só pode ser usado dessa maneira no lado Client-Side, ou seja, no lado do Front. Para conseguir usar o JavaScript no lado do Servidor, você precisa baixar uma ferramenta chamada node.JS, que vai fazar o JavaScript funcionar também no lado BackEnd.
 */
