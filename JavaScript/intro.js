050116aaace045980ceabe973785afb417e494ab
Renaísa <renaisaalves@gmail.com>
2022-07-02 07:44:53 -0300

    Update intro.js

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -1,1 +1,16 @@
-// -> Faz um comentário em JavaScript. 
+// -> Faz um comentário "simples" em JavaScript. É similar ao # do Python.
+/* -> Serve para fazer comentários maiores, com inúmeras linhas. É similar ao ''' ''' do Python. */
+
+// DIFERENÇA ENTRE VARIÁVEL E CONSTANTE
+// Uma variável é um valorque pode ser alterado com o tempo.
+// Já uma constante é um valor que não pode ser alterado, isto é, permanece o mesmo durante todo o tempo (ou execução do programa).
+
+// Vamos conhecer a sintaxe simples do JavaScript usando dois exemplos de variável e constante:
+
+var desconto = 0.2;
+const PRECO = 2;
+
+desconto = PRECO - desconto;
+PRECO = PRECO - desconto; // Isso está errado, pois a constante PRECO não pode ter o seu valor alterado.
+
+
