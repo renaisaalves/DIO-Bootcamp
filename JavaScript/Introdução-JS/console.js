967b8fc3ebfff6da43de8e969e96993d6fc3262a
Renaísa <renaisaalves@gmail.com>
2022-07-02 08:15:06 -0300

    Update console.js

--- JavaScript/Introdução-JS/console.js
+++ JavaScript/Introdução-JS/console.js
@@ -1,1 +1,6 @@
-// 
+/* CONSOLE NUMA PÁGINA DA WEB
+
+Quem disse que o JavaScript só pode ser usado em uma IDE? JavaScript é a linguagem do navegador, portanto, ela pode ser usada diretamente no navegador! 
+
+Para usar a linguagem JavaScript no navegador (vou usar o Google Chrome), basta clicar em "Inspecionar" e depois em "Console". Dessa forma, você pode interagir com a linguagem diretamente no browser, declarando variáveis, constantes, funções, enfim... tudo o que quiser. E o melhor de tudo é que a interpretação ocorre na hora, já que os navegadores são fluentes nessa linguagem.
+*/
