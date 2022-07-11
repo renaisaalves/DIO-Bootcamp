cf97e7f5e836fc1cb4e010f0c8723462c45f829b
Renaísa <renaisaalves@gmail.com>
2022-07-11 19:41:24 -0300

    updates

--- JavaScript/Introdução-JS/4-projetoinicial/4-assets/4-javascript/scripts.js
+++ JavaScript/Introdução-JS/4-projetoinicial/4-assets/4-javascript/scripts.js
@@ -1,18 +1,14 @@
 console.log("Hello World!");
-let count = 0;
 
-const CURRENT_NUMBER = document.getElementById('currentNumber');
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
 
 function increment() {
-	count++;
-	CURRENT_NUMBER.innerHTML = count;
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
-	count--;
-	CURRENT_NUMBER.innerHTML = count;
-}
-
-function test() {
-	kdowkdpo;
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
 }
